a69ec9e9e1193905bf49b7b1f0576c97be44c022
AbdullahMalik7 <abdullahmlk619@gmail.com>
2026-03-12 01:08:53 +0500

    Initial commit

---
+++ app/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "my_math/my_math.h"
+using namespace std;
+using namespace math;
+
+int main(){
+    int a = 10, b = 5;
+
+    cout << "subtraction : " << subtract(a, b) << endl;
+    
+    return 0;
+}
---
+++ include/my_math/my_math.h
@@ -0,0 +1,12 @@
+#ifndef OOP_REPO_2ND_SEM
+#define OOP_REPO_2ND_SEM
+
+namespace math {
+    int add(int a, int b);
+    int subtract(int a, int b);
+    int multiply(int a, int b);
+    float divide(int a, int b);
+    int modulo(int a, int b);
+}
+
+#endif // OOP_REPO_2ND_SEM
